3a256be7882fee00a495e58c10a161d2905257cd
hesam akbari <h.akbari@emofid.com>
2023-08-22 14:29:29 +0330

    create project

---
+++ app/build/generated/source/buildConfig/release/ir/easytrader/orbis/m/twa/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ir.easytrader.orbis.m.twa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "ir.easytrader.orbis.m.twa";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 8;
+  public static final String VERSION_NAME = "8";
+}
---
+++ app/src/main/java/ir/easytrader/orbis/m/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package ir.easytrader.orbis.m.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
